d46d0dfb59d0105f3e8cd31074e9b5700b75ef6a
Saloni231 <ghagsaloni231@gmail.com>
2022-11-29 14:09:57 +0530

    Added Contact Us Page

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,11 +1,58 @@
-import React, { Component } from 'react'
+import React from "react";
+import companyLogo from "./food.jpg";
 
-export class ContactUs extends Component {
-  render() {
-    return (
-      <div>ContactUs</div>
-    )
-  }
+export default function ContactUs() {
+  return (
+    <div class="row" style={{padding: "47px", margin:"20px"}}>
+      <div class="col">
+        <h1>
+          {" "}
+          Request A <font color="#FFFFFF"> Call Back</font>
+        </h1>
+        <br />
+        <form className="ui form">
+          <input
+            class="form-control"
+            id="inputdefault"
+            type="text"
+            placeholder="Name"
+            name="name"
+          />{" "}
+          <br />
+          <input
+            class="form-control"
+            id="inputdefault"
+            placeholder="Phone number"
+            name="phone"
+          />
+          <br />
+          <input
+            class="form-control"
+            id="inputdefault"
+            placeholder="Email Id"
+            name="EMail"
+          />
+          <br />
+          <textarea
+            name="message"
+            cols="55"
+            rows="3"
+            placeholder="Message"
+          ></textarea>
+          <button type="button" class="btn btn-dark" style={{marginTop: "10px"}}>
+            Submit
+          </button>
+        </form>
+      </div>
+      <div class="col">
+        <img
+          src={companyLogo}
+          id="img"
+          className="img-fluid rounded-start"
+          alt="..."
+          style={{width: "400px", height: "350px"}}
+        />
+      </div>
+    </div>
+  );
 }
-
-export default ContactUs
